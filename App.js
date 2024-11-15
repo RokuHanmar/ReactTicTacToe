b16f097e58c27c499455b08670088741bdade277
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-11-15 19:09:07 +0000

    Update App.js

--- App.js
+++ App.js
@@ -87,6 +87,25 @@ export default function Game() {
         setHistory([...history, nextSquares]);
         setXIsNext(!xIsNext);
     }
+
+    function jumpTo(nextMove){
+        //TODO
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = "Go to move # " + move;
+        } else {
+            description = "Go to game start";
+        }
+        return (
+            <li>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
+
     return (
         <div className="game">
             <div className="game-board">
@@ -94,7 +113,7 @@ export default function Game() {
                 <Board/>
             </div>
             <div className="game-info">
-                <ol>{/*TODO*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
