856242290410be442450671616a90edea51b06b5
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-11-15 19:15:44 +0000

    Update App.js

--- App.js
+++ App.js
@@ -78,24 +78,24 @@ function calculateWinner(squares) {
     return null;
 }
 
+// Run the game
 export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState(0);
+    const xIsNex = currentMove %2 === 0;
     const currentSquares = history[currentMove];
 
     function handlePlay(nextSquares){
         const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
         setHistory(nextHistory);
         setCurrentMove(nextHistory.length - 1);
-        setXIsNext(!xIsNext);
     }
 
     function jumpTo(nextMove){
         setCurrentMove(nextMove);
-        setXIsNext(nextMove % 2 === 0);
     }
 
+    // Allows players to replay moves
     const moves = history.map((squares, move) => {
         let description;
         if (move > 0) {
